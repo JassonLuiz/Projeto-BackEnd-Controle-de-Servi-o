a91493eaa7b08211788059606fa93039e2a1efc0
JassonLuiz <jassonluizjr@hotmail.com>
2021-09-22 19:00:13 -0300

    Cria repositorios e conexao com banco de dados h2

---
+++ src/main/java/io/github/jassonluizjr/clientes/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package io.github.jassonluizjr.clientes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.jassonluizjr.clientes.model.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer>{
+
+}
