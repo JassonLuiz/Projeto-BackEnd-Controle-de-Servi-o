fb60f7a417a9297158227507a4609d3d77e7cb15
JassonLuiz <jassonluizjr@hotmail.com>
2022-02-03 14:50:54 -0300

    Autentica com os usuários da base de dados

--- src/main/java/io/github/jassonluizjr/clientes/config/SecurityConfig.java
+++ src/main/java/io/github/jassonluizjr/clientes/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.github.jassonluizjr.clientes.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,21 +8,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import io.github.jassonluizjr.clientes.service.UsuarioService;
 
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-		    .inMemoryAuthentication()
-		    .withUser("Jasson")
-			.password("123")
-			.roles("USER");
+			.userDetailsService(usuarioService)
+			.passwordEncoder(passwordEncoder());
 	}
 	
 	@Bean
+	@Autowired
 	public AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
@@ -33,5 +40,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		    .cors()
 	    .and()
 	    	.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
--- src/main/java/io/github/jassonluizjr/clientes/model/repository/UsuarioRepository.java
+++ src/main/java/io/github/jassonluizjr/clientes/model/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package io.github.jassonluizjr.clientes.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.jassonluizjr.clientes.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
+	Optional<Usuario> findByUsername(String username);
 }
