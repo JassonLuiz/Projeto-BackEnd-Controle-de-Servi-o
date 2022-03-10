17a153bfce759b5edf3023adc8f2329827b11802
JassonLuiz <jassonluizjr@hotmail.com>
2022-03-10 13:30:52 -0300

    Melhorias no registro de Usuário

--- src/main/java/io/github/jassonluizjr/clientes/exception/UsuarioCadastradoException.java
+++ src/main/java/io/github/jassonluizjr/clientes/exception/UsuarioCadastradoException.java
@@ -3,6 +3,6 @@ package io.github.jassonluizjr.clientes.exception;
 public class UsuarioCadastradoException extends RuntimeException{
 
 	public UsuarioCadastradoException(String login) {
-		super("Usuário já cadastrado para o login!" + login);
+		super("Usuário já cadastrado para o login " + login);
 	}
 }
